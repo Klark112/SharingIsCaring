6f55ec1964dad7160427a23759fec449d567bf0c
stuarthoye <hoye@ualberta.ca>
2016-02-10 12:27:13 -0700

    Rough-in Bid object

--- app/src/main/java/sharingiscaring/sharingiscaring/Bid.java
+++ app/src/main/java/sharingiscaring/sharingiscaring/Bid.java
@@ -4,4 +4,28 @@ package sharingiscaring.sharingiscaring;
  * Created by hoye on 2/10/16.
  */
 public class Bid {
+    private User bidder;
+    private double rate;
+
+    public Bid(){
+
+    }
+    public Bid(User bidder, double rate){
+        this.bidder = bidder;
+        this.rate = rate;
+    }
+
+    public User getBidder(){
+        return this.bidder;
+    }
+    public double getRate(){
+        return this.rate;
+    }
+
+    public void setUser(User newBidder){
+        this.bidder = newBidder;
+    }
+    public void setRate(double newRate){
+        this.rate = newRate;
+    }
 }
