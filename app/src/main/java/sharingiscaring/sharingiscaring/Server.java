c0a5503a0ce9cca124133b573de6f498ce4a958e
stuarthoye <hoye@ualberta.ca>
2016-02-10 13:13:32 -0700

    Rough-in of User class and partial Server class.

--- app/src/main/java/sharingiscaring/sharingiscaring/Server.java
+++ app/src/main/java/sharingiscaring/sharingiscaring/Server.java
@@ -1,7 +1,69 @@
 package sharingiscaring.sharingiscaring;
 
+import java.util.ArrayList;
+
 /**
  * Created by hoye on 2/10/16.
  */
 public class Server {
+    private ArrayList<User> users;
+    private int identifier;
+
+    public Server(){
+        this.users = new ArrayList<User>();
+        this.identifier = 0;
+    }
+    public Server(ArrayList<User> users, int identifier){
+        this.users = users;
+        this.identifier = identifier;
+    }
+
+    public ArrayList<User> getUsers(){
+        return this.users;
+    }
+    public int getIdentifier(){
+        return this.identifier;
+    }
+
+    public void setUsers(ArrayList<User> newUsers){
+        this.users = newUsers;
+    }
+    public void setIdentifier(int newID){
+        this.identifier = newID;
+    }
+
+    public void addUser(User user){
+        this.users.add(user);
+    }
+    public int identify(){
+        int retval = this.identifier;
+        identifier += 1;
+        return  retval;
+    }
+
+    public User getUser(String name){
+        for (int i = 0; i < users.size(); i++){
+            User user = users.get(i);
+            if (user.getName() == name){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Item getItem(int identifier){
+        for (int i = 0; i < users.size(); i++){
+            User user = users.get(i);
+            ArrayList<Item> userItems = user.getOwnedItems();
+            for (int j = 0; j < userItems.size(); j++){
+                Item item = userItems.get(j);
+                if (item.getIdentifier() == identifier){
+                    return item;
+                }
+            }
+        }
+        return null;
+    }
+
+
 }
--- app/src/main/java/sharingiscaring/sharingiscaring/User.java
+++ app/src/main/java/sharingiscaring/sharingiscaring/User.java
@@ -1,7 +1,100 @@
 package sharingiscaring.sharingiscaring;
 
+import java.util.ArrayList;
+
 /**
  * Created by hoye on 2/10/16.
  */
 public class User {
+    private String name;
+    private ArrayList<Item> ownedItems;
+    private ArrayList<Item> otherItems;
+    private String contact;
+    private String phone;
+    private String email;
+    private String streetAddress;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String password;
+
+
+    public User(){
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public ArrayList<Item> getOwnedItems(){
+        return this.ownedItems;
+    }
+    public ArrayList<Item> getBorrowedItems(){
+        return this.otherItems;
+    }
+    public String getContact(){
+        return this.contact;
+    }
+    public String getPhone(){
+        return this.phone;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getStreetAddress(){
+        return this.streetAddress;
+    }
+    public String getCity(){
+        return this.city;
+    }
+    public String getProvince(){
+        return this.province;
+    }
+    public String getPostalCode(){
+        return this.postalCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setOwnedItems(ArrayList<Item> newItems){
+        this.ownedItems = newItems;
+    }
+    public void setOtherItems(ArrayList<Item> newItems){
+        this.otherItems = newItems;
+    }
+    public void setContact(String newContact){
+        this.contact = newContact;
+    }
+    public void setPhone(String newPhone){
+        this.phone = newPhone;
+    }
+    public void  setEmail(String newEmail){
+        this.email = newEmail;
+    }
+    public void setStreetAddress(String newAddress){
+        this.streetAddress = newAddress;
+    }
+    public  void setCity(String newCity){
+        this.city = newCity;
+    }
+    public void setProvince(String newProvince){
+        this.province = newProvince;
+    }
+    public void setPostalCode(String newPostalCode){
+        this.postalCode = newPostalCode;
+    }
+
+    public void addItem(Item item){
+        this.ownedItems.add(item);
+    }
+    public void borrow(Item item){
+        this.otherItems.add(item);
+    }
+    public void returnItem(Item item){
+        this.otherItems.remove(item);
+    }
+    public void update(){
+
+    }
 }
