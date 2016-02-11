8fbdd255e63f1ab5915ca8c93cdc07c1eb965f3c
stuarthoye <hoye@ualberta.ca>
2016-02-10 17:02:03 -0700

    Add test cases for UC 01.01.01 & 01.02.01

--- app/src/androidTest/java/sharingiscaring/sharingiscaring/testItem.java
+++ app/src/androidTest/java/sharingiscaring/sharingiscaring/testItem.java
@@ -14,11 +14,23 @@ public class testItem extends ActivityInstrumentationTestCase2{
     public void testDefault(){
         Item item = new Item();
         Server server = new Server();
+
         assertEquals("default", item.getDescription());
         assertEquals("default", item.getTitle());
         assertEquals("default", item.getOwner().getName());
         assertEquals(item.AVAILABLE, item.getStatus());
         assertTrue(item.getBids().isEmpty());
+    }
+
+    public void testEditItem(){
+        Item item = new Item();
+        item.setTitle("Non-default");
+        item.setDescription("Non-default");
+        item.setStatus(Item.BIDDING);
+
+        assertTrue(item.getTitle() == "Non-default");
+        assertTrue(item.getDescription() == "Non-default");
+        assertTrue(item.getStatus() == Item.BIDDING);
     }
 
     public void testAddBid(){
--- app/src/androidTest/java/sharingiscaring/sharingiscaring/testUser.java
+++ app/src/androidTest/java/sharingiscaring/sharingiscaring/testUser.java
@@ -10,4 +10,13 @@ public class testUser extends ActivityInstrumentationTestCase2 {
     public testUser() {
         super(User.class);
     }
+
+    public void testAddItem(){
+        User user = new User();
+        Item item = new Item();
+
+        assertTrue(user.getOwnedItems().isEmpty());
+        user.addItem(item);
+        assertFalse(user.getOwnedItems().isEmpty());
+    }
 }
