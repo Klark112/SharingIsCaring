b331954b4e0187031daff4008beb38822a085d6b
K14RK <K14RK>
2016-02-15 17:06:16 -0700

    Finished adding constructor, getters and setters to pokemon and move class, removed type class, replaced with String object.

--- app/src/main/java/sharingiscaring/sharingiscaring/Move.java
+++ app/src/main/java/sharingiscaring/sharingiscaring/Move.java
@@ -5,9 +5,34 @@ package sharingiscaring.sharingiscaring;
  */
 public class Move {
     private String moveName;
-    // A move has a base multplier that it checks against the attack type
-    private int damageMultiplier;
-    private Type type; // each move has a SINGLE type
+    // A move has a base power that it checks against the attack type
+    // ie. drain punch has a power of 75
+    private int damagePower;
+    private int accuracy;
+    private String type; // each move has a SINGLE type
     private String attackType; //Physical or Special attacks determine
+
+    public Move(String moveName, int damagePower, int accuracy, String type, String attackType) {
+        this.moveName = moveName;
+        this.damagePower = damagePower;
+        this.accuracy = accuracy;
+        this.type = type;
+        this.attackType = attackType;
+    }
+
+    public String getMoveName() {return moveName;}
+    public void setMoveName(String moveName) {this.moveName = moveName; }
+
+    public int getDamagePower() {return damagePower;}
+    public void setDamagePower(int damagePower) {this.damagePower = damagePower;}
+
+    public int getAccuracy() {return accuracy;}
+    public void setAccuracy(int accuracy) {this.accuracy = accuracy;}
+
+    public String getType() {return type;}
+    public void setType(String type) {this.type = type;}
+
+    public String getAttackType() {return attackType;}
+    public void setAttackType(String attackType) {this.attackType = attackType;}
     // whether SpA or Att Stat is used
 }
--- app/src/main/java/sharingiscaring/sharingiscaring/Pokemon.java
+++ app/src/main/java/sharingiscaring/sharingiscaring/Pokemon.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by klark on 2/11/16.
  */
 public class Pokemon extends Item {
-    private ArrayList<Integer> stats;
+    //private ArrayList<Integer> stats; for now i am ignoring EV's and IV's
     //Should we contract the variable names to HP, Att, Def, SpA, SpD, Spe
     private String Ability;
     private int Level;
@@ -16,8 +16,8 @@ public class Pokemon extends Item {
     private int SpecialAttack;
     private int SpecialDefense;
     private int Speed;
-    private Type PrimaryType;
-    private Type SecondaryType;
+    private String PrimaryType;
+    private String SecondaryType;
     private Move move1;
     private Move move2;
     private Move move3;
@@ -25,6 +25,77 @@ public class Pokemon extends Item {
     //should we give nature and item their own class because the impact the stats
     private String nature;
     // talk with group about using items or not due to variety and ease of implementation
-    private String item;
+    private String pokemonItem;
 
+    public Pokemon(int identifier, User owner, String title, String description,
+                   ArrayList<Bid> bids, String ability, int level, int healthPoints, int attack,
+                   int defense, int specialAttack, int specialDefense, int speed,
+                   String primaryType, String secondaryType,Move move1, Move move2,
+                   Move move3, Move move4, String nature, String pokemonItem) {
+        super(identifier, owner, title, description, bids);
+        Ability = ability;
+        Level = level;
+        HealthPoints = healthPoints;
+        Attack = attack;
+        Defense = defense;
+        SpecialAttack = specialAttack;
+        SpecialDefense = specialDefense;
+        Speed = speed;
+        PrimaryType = primaryType;
+        SecondaryType = secondaryType;
+        this.move1 = move1;
+        this.move2 = move2;
+        this.move3 = move3;
+        this.move4 = move4;
+        this.nature = nature;
+        this.pokemonItem= pokemonItem;
+    }
+
+    public String getAbility() {return Ability;}
+    public void setAbility(String ability) {Ability = ability;}
+
+    public int getLevel() {return Level;}
+    public void setLevel(int level) {Level = level;}
+
+    public int getHealthPoints() {return HealthPoints;}
+    public void setHealthPoints(int healthPoints) {HealthPoints = healthPoints;}
+
+    public int getAttack() {return Attack;}
+    public void setAttack(int attack) {Attack = attack;}
+
+    public int getDefense() {return Defense;}
+    public void setDefense(int defense) {Defense = defense;}
+
+    public int getSpecialAttack() {return SpecialAttack;}
+    public void setSpecialAttack(int specialAttack) {SpecialAttack = specialAttack;}
+
+    public int getSpecialDefense() {return SpecialDefense;}
+    public void setSpecialDefense(int specialDefense) {SpecialDefense = specialDefense;}
+
+    public int getSpeed() {return Speed;}
+    public void setSpeed(int speed) {Speed = speed;}
+
+    public String getPrimaryType() {return PrimaryType;}
+    public void setPrimaryType(String primaryType) {PrimaryType = primaryType;}
+
+    public String getSecondaryType() {return SecondaryType;}
+    public void setSecondaryType(String secondaryType) {SecondaryType = secondaryType;}
+
+    public Move getMove1() {return move1;}
+    public void setMove1(Move move1) {this.move1 = move1;}
+
+    public Move getMove2() {return move2;}
+    public void setMove2(Move move2) {this.move2 = move2;}
+
+    public Move getMove3() {return move3;}
+    public void setMove3(Move move3) {this.move3 = move3;}
+
+    public Move getMove4() {return move4;}
+    public void setMove4(Move move4) {this.move4 = move4;}
+
+    public String getNature() {return nature;}
+    public void setNature(String nature) {this.nature = nature;}
+
+    public String getPokemonItem() {return pokemonItem;}
+    public void setItem(String pokemonItem) {this.pokemonItem = pokemonItem;}
 }
