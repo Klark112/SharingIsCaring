7019b7d0b8d0d8fba621338141f58e303972bfa3
klark112 <klark@ualberta.ca>
2016-02-23 20:00:08 -0700

    From Home

--- app/src/main/java/sharingiscaring/sharingiscaring/MainActivity.java
+++ app/src/main/java/sharingiscaring/sharingiscaring/MainActivity.java
@@ -17,16 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+
+    //tell view button where to go
+    public void changes(View v){setContentView(R.layout.my_pc); }
+    //implement view button
+    View.OnClickListener viewButton = new View.OnClickListener() {
+        public void onClick(View v) {
+            setContentView(R.layout.my_pc);
+            (findViewById(R.id.change)).setOnClickListener(viewButton);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
